b006e9756be24886b51fb0798727e395912ab709
theiconik <kumarujjawal51@gmail.com>
2021-05-02 00:01:02 +0530

    Components styling and bootstrap added

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@ import "./App.css";
 import "cirrus-ui";
 import useFetchJobs from "./Hooks/useFetchJobs";
 import { useState } from "react";
-import Job from './Components/Job'
+import Job from "./Components/Job";
 
 function App() {
   const [params, setParams] = useState({});
-  const [page, setPage] = useState(1)
+  const [page, setPage] = useState(1);
   const { jobs, loading, error } = useFetchJobs(params, page);
 
   return (
     <div className="App">
-    {loading && <h1>Loading...</h1>}
-    {error && <h1>Error. Try Refreshing!</h1>}
-    {jobs.map(job => {
-      return <Job key={job.id} job = {job} />
-    })}
+      {loading && (
+        <div class="animated loading hide-text">
+          <p>Hidden</p>
+        </div>
+      )}
+      {error && <h1>Error. Try Refreshing!</h1>}
+      {jobs.map((job) => {
+        return <Job key={job.id} job={job} className="u-flex u-justify-center u-align-center animated fadeIn" />;
+      })}
     </div>
   );
 }
--- src/Components/Job.js
+++ src/Components/Job.js
@@ -1,21 +1,55 @@
 import "cirrus-ui";
-import useFetchJobs from "../Hooks/useFetchJobs";
+import ReactMarkdown from "react-markdown";
 
 const Job = ({ job }) => {
   return (
-    <div class="card">
-      <div class="card__header">
-        <p class="font-bold px-3">{job.title} - <span className="font-light">{ job.company }</span></p>
-      </div>
-      <div class="content">
-        <p>
-          Full time/ Part time
-          <a href="!#">@Cirrus</a>
-        </p>
-      </div>
-      <div class="card__footer level content">6:32 PM - 3 Jul 18</div>
-      <div class="card__action-bar u-center">
-        <button class="btn-transparent outline">View Details</button>
+    <div class="u-flex u-justify-center u-align-center">
+      <div class="card" style={{ maxWidth: "800px", width: "100%" }}>
+        <div>
+          <div class="card__header">
+            <p class="font-bold px-3">
+              {job.title} -{" "}
+              <a class="font-thin" href="!#">
+                @ {job.company}
+              </a>
+            </p>
+          </div>
+          <div className="u-flex u-justify-space-between">
+            <div class="content u-text-left" style={{ margin: "25px" }}>
+              <div
+                class="tag-container group-tags"
+                style={{ marginTop: "10px" }}
+              >
+                <div class="tag tag--dark">{job.type}</div>
+                <div class="tag tag--primary">{job.location}</div>
+              </div>
+              <div class="" style={{ wordBreak: "break-all" }}>
+                <ReactMarkdown source={job.how_to_apply} />
+              </div>
+            </div>
+            <div>
+              <img
+                src={job.company_logo}
+                style={{
+                  height: "50px",
+                  position: "relative",
+                  top: "50%",
+                  right: "25%",
+                  zIndex: "0",
+                }}
+                alt={job.company}
+              />
+            </div>
+          </div>
+          <div class="card__footer level" style={{ margin: "25px" }}>
+            {new Date(job.created_at).toLocaleDateString()}
+          </div>
+          <div class="card__action-bar u-center">
+            <button class="outline btn-info btn-small hover-grow" style={{margin:"5px 0"}}>
+              View Details
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
